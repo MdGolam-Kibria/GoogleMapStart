27913cefc13a0f314a9c3d6053be8788485af53a
Kibria92 <kibria92@gmail.com>
2020-05-31 23:58:55 +0600

    in this repository i show a location using lat lng

---
+++ app/src/main/java/com/example/googlemapstart/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.googlemapstart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+
+        LatLng latLng = new LatLng(23.754303, 90.360690);
+        mMap.addMarker(new MarkerOptions().position(latLng).title("dhanmodi lake by kibria"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+    }
+}
